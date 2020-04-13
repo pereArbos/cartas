15d6f925db7facdda450eb87b5f45b85453c13f0
perearbos <arbospp4f2010@gmail.com>
2020-04-13 21:52:47 +0200

    fix

--- src/game/playerArea/iconFooter/IconFooter.js
+++ src/game/playerArea/iconFooter/IconFooter.js
@@ -1,24 +1,22 @@
 import React from 'react';
 
-export default class DeckZone extends React.Component {
-  getIconValue = (image, value, left) => {
-    return [
-      <div className="IconFooter" style={{ left: `${left}vw` }}>
-        <img alt="noseve" src={require(`./icons/${image}`)} />
-      </div>,
-      <div
-        className="IconFooter"
-        style={{ backgroundColor: '#ffadd7', left: `${left + 3}vw` }}
-      >
-        {value || 0}
-      </div>,
-    ];
-  };
+export default function IconFooter(props) {
+  const { data, firstLeft } = props;
+  return data.map((item, idx) => {
+    return getIconValue(item[0], item[1], firstLeft + idx * 6);
+  });
+}
 
-  render() {
-    const { data, firstLeft } = this.props;
-    return data.map((item, idx) => {
-      return this.getIconValue(item[0], item[1], firstLeft + idx * 6);
-    });
-  }
+function getIconValue(image, value, left) {
+  return [
+    <div className="IconFooter" style={{ left: `${left}vw` }}>
+      <img alt="noseve" src={require(`./icons/${image}`)} />
+    </div>,
+    <div
+      className="IconFooter"
+      style={{ backgroundColor: '#ffadd7', left: `${left + 3}vw` }}
+    >
+      {value || 0}
+    </div>,
+  ];
 }
